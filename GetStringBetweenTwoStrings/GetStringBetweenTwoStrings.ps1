d04665a0bc41bfe67daa1f3f4d922990bc4aeac2
chunk1ty <andriyan.krastev@gmail.com>
2021-08-13 14:53:28 +0300

    modify extarcting script

--- GetStringBetweenTwoStrings/GetStringBetweenTwoStrings.ps1
+++ GetStringBetweenTwoStrings/GetStringBetweenTwoStrings.ps1
@@ -1,19 +1,21 @@
 # calling .\GetStringBetweenTwoStrings.ps1 -FilePath C:\andriyan\personal\repositories\PowershellPlayground\GetStringBetweenTwoStrings\input.txt -FirstString 'account_id":' -SecondString ',"type"'
 param (
-    [Parameter(Mandatory=$True)][string] $FilePath, 
     [Parameter(Mandatory=$True)][string] $FirstString, 
     [Parameter(Mandatory=$True)][string] $SecondString
 )
 
-$file = Get-Content -Path $FilePath
+$file = Get-Content -Path C:\andriyan\personal\repositories\PowershellPlayground\GetStringBetweenTwoStrings\input.txt
 
 $result
-foreach ($line in $file)
-{
-    $startIndex = $line.IndexOf($FirstString) + $FirstString.Length
+foreach ($line in $file){
+    $firstStringIndex = $line.IndexOf($FirstString)
+    if ($firstStringIndex -eq -1) {
+        continue
+    }
+    $startIndex = $firstStringIndex + $FirstString.Length
     $length = $line.IndexOf($SecondString) - $startIndex
     
-    $result += $line.Substring($startIndex, $length)
+    $result += $line.Substring($startIndex, $length).Trim()
     $result += [Environment]::NewLine
 }
 
